800641b4a71f73fe8c68b7f446eedb95cc61edd8
jay <jay.wang.scorpion@gmail.com>
2016-08-18 11:34:40 +0800

    :smile: init

---
+++ README.md
@@ -0,0 +1,33 @@
+# one h5
+
+## What
+
+one h5 demo
+
+## Use
+-	react
+-	react-redux
+-	react-thunk
+-	react-router
+-	webpack
+-	fetch
+-	sass
+
+## How
+
+1. install
+   
+	npm install
+
+2. run
+
+	webpack-dev-server --progress --colors
+
+	open browser http://localhost:8080/webpack-dev-server/
+
+3. package
+	
+	webpack
+
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Routers from './routers'
+import todoApp from './reducers/reducers'
+
+import { Router, Route, hashHistory } from 'react-router'
+
+import configureStore from './stores';
+
+require('./sass/_main')
+
+const store = configureStore();
+render(
+  <Provider store={store}>
+    <Router history={hashHistory} routes={Routers} />
+  </Provider>,
+  document.getElementById('root')
+)
